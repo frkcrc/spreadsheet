755e11f13c2e4ce4b310ba44a1babf199c6ffe1b
frkcrc <frkcrc@gmail.com>
2022-12-13 12:23:46 +0100

    Remove Spacer component

--- src/components/Spreadsheet.js
+++ src/components/Spreadsheet.js
@@ -3,7 +3,6 @@ import RowHeadings from './headings/RowHeadings';
 import VScrollbar from './scrollbars/VScrollbar';
 import HScrollbar from './scrollbars/HScrollbar';
 import SheetPane from './sheet-view/SheetView';
-import Spacer from './Spacer';
 
 import styles from './Spreadsheet.module.css';
 
@@ -12,9 +11,7 @@ const Spreadsheet = () => {
     <>
 
       <div className={styles.header}>
-        <Spacer width={50} />
         <ColHeadings />
-        <Spacer width={30} />
       </div>
 
       <div className={styles.content}>
@@ -24,9 +21,7 @@ const Spreadsheet = () => {
       </div>
 
       <div className={styles.footer}>
-        <Spacer width={50} />
         <HScrollbar />
-        <Spacer width={30} />
       </div>
     </>
   );
