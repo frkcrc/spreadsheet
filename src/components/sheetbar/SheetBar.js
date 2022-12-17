fa909f6450e52ddfb887458c05f9afaaeaa8dca2
frkcrc <frkcrc@gmail.com>
2022-12-17 13:49:45 +0100

    SheetBar: Add the add button

--- src/components/sheetbar/SheetBar.js
+++ src/components/sheetbar/SheetBar.js
@@ -3,7 +3,9 @@ import styles from './SheetBar.module.scss';
 const SheetBar = () => {
   return (
     <div className={styles.sheetBar}>
-        
+        <div className={styles.addButton}>
+          <button type="button">+</button>
+        </div>
     </div>
   )
 };
