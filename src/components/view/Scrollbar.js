2eebd9a087cc87efb825343116b271e23456adbf
frkcrc <frkcrc@gmail.com>
2022-12-19 14:36:13 +0100

    Scrollbar: Add responsive length calculation

--- src/components/view/Scrollbar.js
+++ src/components/view/Scrollbar.js
@@ -1,15 +1,17 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useLayoutEffect } from 'react';
 import styles from './Scrollbar.module.scss';
 
 const Scrollbar = props => {
 
   const scrollbarRef = useRef();
+  const [length, setLength] = useState(0);
   const [lastPos, setLastPos] = useState(0);
   const [offset, setOffset] = useState(0);
   const [pointerId, setPointerId] = useState(undefined);
 
   const {axis, size} = props;
 
+  // Calculate dimensions and style the handler.
   const handleStyle = {};
   if (axis === 'x') {
     handleStyle.width = size;
@@ -18,6 +20,19 @@ const Scrollbar = props => {
     handleStyle.height = size;
     handleStyle.top = offset + 'px';
   }
+
+  // Effect to update the length on render and on resize.
+  useLayoutEffect(() => {
+    const calculate = () => {
+      setLength((axis === 'x' ? 
+        scrollbarRef.current.clientWidth : 
+        scrollbarRef.current.clientHeight )
+      );
+    };
+    calculate();
+    window.addEventListener('resize', calculate);
+    return _ => window.removeEventListener('resize', calculate);
+  }, [axis, scrollbarRef]);
 
   const startDraggingHandler = event => {
     setPointerId(event.pointerId);
@@ -36,10 +51,7 @@ const Scrollbar = props => {
     if (!pointerId) return;
     const currentPos = 
       (axis === 'x' ? event.clientX : event.clientY);
-    const maxOffset = 
-      (axis === 'x' ? 
-        scrollbarRef.current.clientWidth - size - 8 : 
-        scrollbarRef.current.clientHeight - size - 8 );
+    const maxOffset = length - size - 8; // 8 = padding in the trough.
     const delta = currentPos - lastPos;
     setOffset((oldOffset) => {
       const newOffset = oldOffset+delta;
