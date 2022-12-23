4f4fb549c9fa8772ae2d2b82c55491f22fa06a0c
frkcrc <frkcrc@gmail.com>
2022-12-23 14:59:28 +0100

    Refactor state reducer code into helper function

--- src/helpers/view-state.js
+++ src/helpers/view-state.js
@@ -1,8 +1,7 @@
 // Functions that transform pieces of state related to the view.
 
-// Changes a sheet view object, fixing the starting points based on the
-// offset values.
-export function offsetsToStarts(view) {
+// Fixes the starting points based on the offset values.
+export function fixStartingPoints(view) {
   for (let i = 0; i < view.rows.boundaries.length; i++) {
     if (view.rows.boundaries[i] > view.rows.offset)
       break;
@@ -13,4 +12,14 @@ export function offsetsToStarts(view) {
       break;
     view.cols.start = i;
   }
+}
+
+// Adds the given offset, clamping to valid values.
+export function changeOffset(state, axis, delta) {
+  const view = state.sheets[state.selected].view[axis];
+  const length = state.viewport[(axis === 'rows' ? 'height' : 'width')];
+  const maxOffset = view.total - length;
+  view.offset = 
+    Math.min(maxOffset, Math.max(0, view.offset + delta));
+  fixStartingPoints(state.sheets[state.selected].view);
 }
--- src/store/spreadsheet.js
+++ src/store/spreadsheet.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Sheet } from "../helpers/sheet";
-import { offsetsToStarts } from "../helpers/view-state";
+import { changeOffset, fixStartingPoints, offsetsToStarts } from "../helpers/view-state";
 
 const spreadsheetSlice = createSlice({
 
@@ -27,13 +27,8 @@ const spreadsheetSlice = createSlice({
     },
 
     addOffset: (state, action) => {
-      const id = state.selected;
-      const axis = state.sheets[id].view[action.payload.axis];
-      const dir =(action.payload.axis === 'rows' ? 'height' : 'width');
-      const maxOffset = axis.total - state.viewport[dir];
-      axis.offset += action.payload.delta;
-      axis.offset = Math.min(maxOffset, Math.max(0, axis.offset));
-      offsetsToStarts(state.sheets[id].view);
+      const { axis, delta } = action.payload;
+      changeOffset(state, axis, delta);
     },
     
   }
