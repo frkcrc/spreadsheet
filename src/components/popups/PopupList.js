0acd5ae101050be3b1af899f8966fcdf5a129413
frkcrc <frkcrc@gmail.com>
2022-12-27 16:49:28 +0100

    Add popup list and item components skeletons

---
+++ src/components/popups/PopupList.js
@@ -0,0 +1,10 @@
+import styles from './PopupList.module.scss';
+
+const PopupList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+export default PopupList;
---
+++ src/components/popups/PopupListItem.js
@@ -0,0 +1,10 @@
+import styles from './PopupListItem.module.scss';
+
+const PopupListItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+export default PopupListItem;
