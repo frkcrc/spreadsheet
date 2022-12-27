3f1a4abcb72d44912a5f5eb5179d13a755478fae
frkcrc <frkcrc@gmail.com>
2022-12-27 09:45:29 +0100

    Make dummy popup show conditionally

--- src/components/popups/PopupContainer.js
+++ src/components/popups/PopupContainer.js
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import styles from './PopupContainer.module.scss';
 
 const PopupContainer = () => {
+
+  const show = useSelector(state => state.spreadsheet.popup.show);
+
   return (
     <div className={styles.popupContainer}>
-      <div className={styles.popupActive}>
+      <div className={show ? styles.popupActive : styles.popup}>
         Test popup.
       </div>
     </div>
