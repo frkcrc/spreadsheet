0e808883029922f2d28895d4e0efbdbea36cd1ee
frkcrc <frkcrc@gmail.com>
2022-12-27 09:31:25 +0100

    Add event handler to cancel the popup on pointer down

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import SheetBar from './components/sheetbar/SheetBar';
 import SheetPane from './components/SheetPane';
 import PopupContainer from './components/popups/PopupContainer';
 
 import styles from './App.module.scss';
+import { spreadsheetActions } from './store/spreadsheet';
 
 function App() {
+
+  useEffect(() => {
+    const cancelPopup = () => {
+      spreadsheetActions.setPopup({ show: false, data: null });
+    };
+    document.addEventListener('pointerdown', cancelPopup);
+    return _ => 
+      document.removeEventListener('pointerdown', cancelPopup);
+  }, []);
+
   return (
     <>
       <div className={styles.mainContainer}>
