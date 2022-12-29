8359c2b145dd2730e0caac52b70a719bb274c7e1
frkcrc <frkcrc@gmail.com>
2022-12-29 15:21:27 +0100

    ColPopup: Prepare wiring for actions

--- src/components/popups/ColPopup.js
+++ src/components/popups/ColPopup.js
@@ -1,20 +1,47 @@
+import { useDispatch } from "react-redux";
 import PopupList from "./PopupList";
 import PopupListItem from "./PopupListItem";
 
-const ColPopup = () => {
+const ColPopup = props => {
+
+  const dispatch = useDispatch();
+  const column = props.payload.col;
+
+  // Handlers.
+
+  const addLeft = () => {
+    
+  };
+
+  const addRight = () => {
+
+  };
+
+  const removeCol = () => {
+
+  };
+
+  const clearCol = () => {
+
+  };
+
   return (
     <PopupList>
       <PopupListItem
         label="Add Column Left"
+        onClick={addLeft}
       />
       <PopupListItem
         label="Add Column Right"
+        onClick={addRight}
       />
       <PopupListItem
         label="Remove Column"
+        onClick={removeCol}
       />
       <PopupListItem
         label="Clear Column"
+        onClick={clearCol}
       />
     </PopupList>
   )
