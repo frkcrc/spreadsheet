d83cb992f3f51be9b3e4cc386543db903194e824
frkcrc <frkcrc@gmail.com>
2023-01-05 21:22:24 +0100

    Add consts for min width/height for columns and rows

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,8 @@
 export const defaultWidth = 100;
 export const defaultHeight = 30;
 export const rowHeadWidth = 50;
+export const minRowHeight = defaultHeight;
+export const minColWidth = 50;
 export const startingRows = 100;
 export const startingCols = 26;
 export const scrollbarThickness = 30;
