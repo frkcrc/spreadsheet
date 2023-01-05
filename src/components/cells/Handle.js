ec44cf580a5197cf3079c8a9e96ef72596d81fa1
frkcrc <frkcrc@gmail.com>
2023-01-05 21:06:07 +0100

    Add basic head handle drag behavior

--- src/components/cells/Handle.js
+++ src/components/cells/Handle.js
@@ -1,13 +1,55 @@
+import { useState } from 'react';
 import styles from './Handle.module.scss';
 
 const Handle = props => {
 
-  const type = props.type;
+  const [pointerId, setPointerId] = useState(undefined);
+  const [startPos, setStartPos] = useState(undefined);
+  const [handlerOffset, setHandlerOffset] = useState(0);
 
+  const type = props.type;
+  const isCol = type === 'col';
   const className = styles[`handle${type}`];
+  const inlineStyles = {};
+  if (isCol) inlineStyles.right = `${-handlerOffset}px`;
+  else inlineStyles.bottom = `${-handlerOffset}px`;
+
+  // Handlers.
+  const startDraggingHandler = e => {
+    setStartPos(isCol ? e.clientX : e.clientY);
+    setPointerId(e.pointerId);
+    e.target.setPointerCapture(e.pointerId);
+  };
+
+  const stopDraggingHandler = e => {
+    if (pointerId !== undefined)
+      e.target.releasePointerCapture(pointerId);
+    setPointerId(undefined);
+    setHandlerOffset(0);
+    // TODO: Actual resizing of the row/col here.
+  };
+
+  const draggingHandler = e => {
+    if (pointerId === undefined) return;
+    if (isCol) {
+      setHandlerOffset(e.clientX - startPos);
+    } else {
+      setHandlerOffset(e.clientY - startPos);
+    }
+  };
 
   return (
-    <div className={className}></div>
+    <div 
+      className={className}
+      style={inlineStyles}
+      onPointerDown={startDraggingHandler}
+      onPointerUp={stopDraggingHandler}
+      onPointerCancel={stopDraggingHandler}
+      onContextMenu={stopDraggingHandler} // To stop dragging on right click.
+      onPointerMove={draggingHandler}
+    >
+
+    </div>
   );
 };
 export default Handle;
