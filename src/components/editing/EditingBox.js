265bacee731a219bb9c3379b5f4e1e973ce717ac
frkcrc <frkcrc@gmail.com>
2023-01-06 19:44:41 +0100

    EditBox: Autofocus and quit editing on blur

--- src/components/editing/EditingBox.js
+++ src/components/editing/EditingBox.js
@@ -1,10 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { spreadsheetActions } from '../../store/spreadsheet';
 import styles from './EditingBox.module.scss';
 
 const EditingBox = props => {
 
+  const editRef = useRef();
+  const dispatch = useDispatch();
+
   const {cell, x, y} = 
     useSelector(state => state.spreadsheet.editing);
+
+  // Effect to autofocus on the input.
+  useEffect(() => {
+    editRef.current.focus();
+  }, [editRef]);
 
   // Define position with inline styles.
   const inlineStyles = {
@@ -14,10 +24,22 @@ const EditingBox = props => {
     height: `${cell.height}px`,
   };
 
+  // Handlers.
+  const onBlur = e => {
+    dispatch(spreadsheetActions.quitEditing());
+  };
+
+  const keyHandler = e => {
+    
+  }
+
   return (
     <input
+      ref={editRef}
       className={styles.editbox}
       style={inlineStyles}
+      onBlur={onBlur}
+      onKeyDown={keyHandler}
     />
   )
 }
