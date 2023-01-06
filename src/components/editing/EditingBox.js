39238a927ed70714ab47cab15ed14fd552afd287
frkcrc <frkcrc@gmail.com>
2023-01-06 17:31:20 +0100

    Basic input EditingBox

--- src/components/editing/EditingBox.js
+++ src/components/editing/EditingBox.js
@@ -1,11 +1,24 @@
+import { useSelector } from 'react-redux';
 import styles from './EditingBox.module.scss';
 
 const EditingBox = props => {
 
+  const {cell, x, y} = 
+    useSelector(state => state.spreadsheet.editing);
+
+  // Define position with inline styles.
+  const inlineStyles = {
+    top: y,
+    left: x,
+    width: `${cell.width}px`,
+    height: `${cell.height}px`,
+  };
+
   return (
-    <div className={styles.editbox}>
-      
-    </div>
+    <input
+      className={styles.editbox}
+      style={inlineStyles}
+    />
   )
 }
 export default EditingBox;
