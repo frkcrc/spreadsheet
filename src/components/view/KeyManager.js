713347241ba0303e5d8da052a90b335d51768905
frkcrc <frkcrc@gmail.com>
2023-01-06 19:49:34 +0100

    KeyManager: Remove useless editing check

--- src/components/view/KeyManager.js
+++ src/components/view/KeyManager.js
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { spreadsheetActions } from "../../store/spreadsheet";
 
 // Handles general keystrokes events on the UI.
 const KeyManager = () => {
 
   const dispatch = useDispatch();
-  const isEditing = useSelector(state => !!state.spreadsheet.editing.cell);
 
   // Effect to handle keystrokes outside of editing mode.
   useEffect(() => {
     const keyHandler = e => {
       const key = e.key;
       // Handle arrow keys to move selection.
-      if (!isEditing && key.startsWith('Arrow')) {
+      if (key.startsWith('Arrow')) {
         let rowDelta = 0, colDelta = 0;
         if      (key === 'ArrowUp')    rowDelta = -1;
         else if (key === 'ArrowDown')  rowDelta = +1;
@@ -25,7 +24,7 @@ const KeyManager = () => {
     document.addEventListener('keydown', keyHandler);
     return _ => 
       document.removeEventListener('keydown', keyHandler);
-  }, [dispatch, isEditing]);
+  }, [dispatch]);
 
 
   return null;
