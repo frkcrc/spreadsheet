2a24cf00d841111917b8416d68957539a1809bc5
frkcrc <frkcrc@gmail.com>
2023-01-07 19:50:02 +0100

    Add GH ribbon to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,22 @@ const Header = () => {
 
   return (
     <div className={styles.header}>
-      <h1>SPREADSHEET</h1>
+      <div className={styles.title}>
+        <h1>SPREADSHEET DEMO</h1>
+      </div>
+      <div className={styles.ribbon}>
+        <a href="https://github.com/frkcrc/spreadsheet" target="_blank" rel="noreferrer">
+          <img 
+            decoding="async"
+            loading="lazy"
+            width="149"
+            height="149"
+            src="https://github.blog/wp-content/uploads/2008/12/forkme_right_orange_ff7600.png?resize=149%2C149"
+            alt="Fork me on GitHub"
+            data-recalc-dims="1"
+          />
+        </a>
+      </div>
     </div> 
   )
 };
