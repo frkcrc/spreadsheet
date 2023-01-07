7a9b6670c1ffdeb4f0acf358c293e6274ec931cc
frkcrc <frkcrc@gmail.com>
2023-01-07 12:12:33 +0100

    Add row/col clear/del on cell popup

--- src/components/popups/CellPopup.js
+++ src/components/popups/CellPopup.js
@@ -1,14 +1,53 @@
+import { useDispatch } from "react-redux";
+import { spreadsheetActions } from "../../store/spreadsheet";
 import PopupList from "./PopupList";
 import PopupListItem from "./PopupListItem";
 
-const CellPopup = () => {
+const CellPopup = props => {
+
+  const dispatch = useDispatch();
+  const {row, col} = props.payload;
+
+  const removeRow = () => {
+    dispatch(spreadsheetActions.removeRow(row));
+  };
+
+  const clearRow = () => {
+    dispatch(spreadsheetActions.clearRow(row));
+  };
+
+  const removeCol = () => {
+    dispatch(spreadsheetActions.removeColumn(col));
+  };
+
+  const clearCol = () => {
+    dispatch(spreadsheetActions.clearColumn(col));
+  };
+
+
   return (
     <PopupList>
       <PopupListItem
-        label="Sample Item"
+        label="Clear Cell"
       />
       <PopupListItem
-        label="Anotehr Sample Item"
+        label="Clear Selection"
+      />
+      <PopupListItem
+        label="Clear Row"
+        onClick={clearRow}
+      />
+      <PopupListItem
+        label="Clear Column"
+        onClick={clearCol}
+      />
+      <PopupListItem
+        label="Remove Row"
+        onClick={removeRow}
+      />
+      <PopupListItem
+        label="Remove Column"
+        onClick={removeCol}
       />
     </PopupList>
   )
